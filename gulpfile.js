5b9fbb551b2e448937ceb1d770a29eded3f4cf0d
Benjamin White <benjamin.e.white1@gmail.com>
2014-05-18 03:11:06 -0400

    build working and u1 markdown cleaned

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,8 @@ var gutil = require('gulp-util');
 
 source = {
 	assignments: './units/**/lessons/**/assignments/*.md',
-	courseIntro: './intro.md',
-	unitIntros: './units/**/intro.md',
-	lessonIntros: './units/**/lessons/**/intro.md',
-	assets: './units/**/lessons/**/assignments/assets/*.{jpg,png}'
+	assets: './units/**/lessons/**/assignments/assets/*.{jpg,png}',
+	intros: ['./intro.md', './units/**/intro.md', './units/**/lessons/**/intro.md']
 };
 
 build = {
@@ -16,11 +14,27 @@ build = {
 	xmlFolder: './build/xmlPartials/'
 };
 
-gulp.task('buildXML', function () {
+gulp.task('default', ['gatherAssets', 'buildIntros', 'buildAssignments']);
+
+gulp.task('buildAssignments', function () {
     return gulp.src(source.assignments)
-    	.pipe(marked())
+    	.pipe(marked({
+    		renderer: rendererConfig,
+    		// highlight: function (code, lang) {
+    		// 	gutil.log(code);	
+   			// 	 return require('highlight.js').highlight(code, lang).value; // need to get this working, circumvent highlight in ang directive altogether
+  				// }
+    	}))
         .pipe(gulp.dest(build.xmlFolder)); 
 });
+
+gulp.task('buildIntros', function() {
+	return gulp.src(source.intros)
+		.pipe(marked({
+			renderer: rendererConfig
+		}))
+		.pipe(gulp.dest(build.xmlFolder));
+})
 
 gulp.task('gatherAssets', function(){
   return gulp.src(source.assets)
@@ -28,41 +42,52 @@ gulp.task('gatherAssets', function(){
     .pipe(gulp.dest(build.assetsFolder));
 });
 
-function escapeCode(code) {
-	gutil.log(code);
-	return code.replace(/>/g, '&gt;')
-		.replace(/</g, '&lt;')
-		.replace(/&/g, '&amp;');
+function wrapInTags(text, open, close) {
+	return open + text + close;
+}
+
+function wrapAssignmentXML(text) {
+	return wrapInTags('<content>\n' + text + '</content>')
 }
 
 
+function escape(html, encode) {
+  return html
+    .replace(!encode ? /&(?!#?\w+;)/g : /&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+}
+
 var rendererConfig = {
 	
-	// heading: function (text, level) {
+	heading: function (text, level) {
 		
-	// 	var tags = ['<h' + level + '>', '</h' + level + '>'];
+		var tags = ['<h' + level + '>', '</h' + level + '>\n'];
 		
-	// 	if (level===1) {
-	// 		tags = ['<name>', '</name>']
-	// 	}
+		if (level===1) {
+			tags = ['<name>', '</name>\n']
+		}
 		
-	// 	else if (level==2) {
-	// 		tags = ['<header>', '</header>']
-	// 	}
+		else if (level==2) {
+			tags = ['<header>', '</header>\n']
+		}
 		
-	// 	return tags[0] + text + tags[1];
-	// },
-	// // image: function(href, title, text) {
-	// // 	return '<p><img source="' + href + '" asset></p>';
-	// // },
+		return tags[0] + text + tags[1];
+	},
+	image: function(href, title, text) {
+		return '<p><img source="' + href + '" asset></p>';
+	},
 
-	// // link: function(href, title, text) {
-	// // 	return '<a href="' + href + '" target="_blank">' + text + '</a>';
-	// // },
+	link: function(href, title, text) {
+		return '<a href="' + href + '" target="_blank">' + text + '</a>';
+	},
 	
-	// // code: function(code, language) {
-	// // 	return '<codesnippet language="' + language + '>' + code + '</codesnippet>';
-	// // },
+	code: function(code, language) {
+		
+		return '<codesnippet language="' + language + '">' + escape(code) + '</codesnippet>\n';
+	},
 
 	// if header and stripped is equal to "Comprehension Check", then add comprehension check class to it
 	// then we just style this. target sibling ul/ol of h4s with 'comprehension-check' class
