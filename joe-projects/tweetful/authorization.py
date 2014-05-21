af570b7db663945d14203cfec5867ea5ea6a5b06
Joe Turner <joe@oampo.co.uk>
2014-05-21 13:37:58 +0100

    Add draft of Twitter client project.

---
+++ joe-projects/tweetful/authorization.py
@@ -0,0 +1,64 @@
+import json
+import urlparse
+
+import requests
+from requests_oauthlib import OAuth1
+
+from secret import CLIENT_KEY, CLIENT_SECRET
+from urls import *
+
+def get_request_token():
+    oauth = OAuth1(CLIENT_KEY, client_secret=CLIENT_SECRET)
+    response = requests.post(API_URL + REQUEST_TOKEN_URL,
+                             auth=oauth)
+    credentials = urlparse.parse_qs(response.content)
+
+    request_token = credentials.get("oauth_token")[0]
+    request_secret = credentials.get("oauth_token_secret")[0]
+    return request_token, request_secret
+
+def authorize(request_token):
+    authorize_url = API_URL + AUTHORIZE_URL
+    authorize_url = authorize_url.format(request_token=request_token)
+    print 'Please go here and authorize: ' + authorize_url
+    return raw_input('Please input the verifier: ')
+
+def get_access_token(request_token, request_secret, verifier):
+    oauth = OAuth1(CLIENT_KEY,
+                   client_secret=CLIENT_SECRET,
+                   resource_owner_key=request_token,
+                   resource_owner_secret=request_secret,
+                   verifier=verifier)
+
+    response = requests.post(API_URL + ACCESS_TOKEN_URL, auth=oauth)
+    credentials = urlparse.parse_qs(response.content)
+    access_token = credentials.get('oauth_token')[0]
+    access_secret = credentials.get('oauth_token_secret')[0]
+    return access_token, access_secret
+
+def store_credentials(access_token, access_secret):
+    with open("access.json", "w") as f:
+        json.dump({"access_token": access_token,
+                   "access_secret": access_secret}, f)
+
+def get_stored_credentials():
+    with open("access.json", "r") as f:
+        credentials = json.load(f)
+        return credentials["access_token"], credentials["access_secret"]
+
+def get_auth():
+    try:
+        access_token, access_secret = get_stored_credentials()
+    except IOError:
+        request_token, request_secret = get_request_token()
+        verifier = authorize(request_token)
+        access_token, access_secret = get_access_token(request_token,
+                                                       request_secret,
+                                                       verifier)
+        store_credentials(access_token, access_secret)
+
+    oauth = OAuth1(CLIENT_KEY,
+                   client_secret=CLIENT_SECRET,
+                   resource_owner_key=access_token,
+                   resource_owner_secret=access_secret)
+    return oauth
---
+++ joe-projects/tweetful/urls.py
@@ -0,0 +1,7 @@
+API_URL = "https://api.twitter.com"
+REQUEST_TOKEN_URL = "/oauth/request_token"
+AUTHORIZE_URL = "/oauth/authorize?oauth_token={request_token}"
+ACCESS_TOKEN_URL = "/oauth/access_token"
+TIMELINE_URL = "/1.1/statuses/home_timeline.json"
+TWEET_URL = "/1.1/statuses/update.json"
+
