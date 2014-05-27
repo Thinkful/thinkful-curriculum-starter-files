3155cde7eb73833b2d0fc2bf3a1d64fb00c19040
Iain <iduncan@xornot.com>
2014-05-27 10:10:16 -0700

    revised version of code along script

--- iain-notes/unit_3/4_code_along/pets_script.py
+++ iain-notes/unit_3/4_code_along/pets_script.py
@@ -38,53 +38,74 @@ from pets_model import (
 class PetApp(object):
 
     def __init__(self, db_url):
-        "connect to the database and get an SQLA session"
-        self.engine = create_engine(db_url, echo=False)
-        self.Session = sessionmaker(bind=self.engine)
-        self.dbs= self.Session()
+        """
+        Create and initialize the PetApp object, creates
+        an sqlalchemy engine and session for the database.
+        Initializes self._dbs, the SQLAlchemy session.
+        param db_url: a db connection string
+        """
+        engine = create_engine(db_url, echo=False)
+        Session = sessionmaker(bind=engine)
+        self._dbs = Session()
 
+    def __delete__(self):
+        self.clean_up()
+
+    def clean_up(self):
+        self._dbs.close()
 
     def search(self, field_args):
         """
-        top level method to search for pets from input args"
-        returns a formatted string list of pets to the terminal
+        Search for pets from a list of search terms.
+        
+        param field_list: a list of strings such as:
+            ['name:titchy','breed:tabby']
+        returns: string output from the search with pet details.
         """
-        print "Searching for pets: %s" % field_args
-        filter_dict = self.fields_to_dict(field_args)
-        pets = self.get_pets(filter_dict)
-        output = self.output_pet_list(pets)
+        filter_dict = self._fields_to_dict(field_args)
+        pets = self._get_pets(filter_dict)
+        output = self._output_pet_list(pets)
+
+        self.clean_up()
         return output
 
 
     def add_pet(self, field_args):
         """
-        top level method to add a new pet to the db
-        - creates breed, species, & shelter if needed
-        - returns string output with success message and pet name
-        - NB: does not yet have error handling
+        Add a new pet to the database. If a breed, shelter,
+        or species is specified, create new ones if they 
+        don't already exist.
+        
+        param field_list: a list of strings such as:
+        side effects: creates a new pet, and possibly a new
+           breed, species, and/or shelter.
+        returns: string output with new pet details.
+            ['name:titchy','breed:tabby']
         """
-        print "Adding pet to database: %s" % field_args
+ 
         # convert fields to a dict of key/val pairs 
-        fields = self.fields_to_dict(field_args)
+        fields = self._fields_to_dict(field_args)
         
         # replace the value in the fields dict with the SQLA species obj or None
-        fields['species'] = self.get_species( fields['species'] ) if \
+        fields['species'] = self._get_species( fields['species'] ) if \
             'species' in fields else None 
-            
-        fields['breed'] = self.get_breed( fields['breed'], species ) if \
+        
+        fields['breed'] = self._get_breed( fields['breed'], species ) if \
             'breed' in fields else None 
 
-        fields['shelter'] = self.get_shelter( fields['shelter'] ) if \
+        fields['shelter'] = self._get_shelter( fields['shelter'] ) if \
             'shelter' in fields else None
         # all our relations are now either None or SQLA objects
         
-        pet = self.save_pet( fields )
-        output = self.output_new_pet(pet)
+        pet = self._save_pet( fields )
+        output = self._add_output(pet)
+        
+        self.clean_up()
         return output
 
 
     @staticmethod
-    def fields_to_dict(field_args):
+    def _fields_to_dict(field_args):
         "convert command line field arguments into a dictionary"
         field_dict = {}
         for field in field_args:
@@ -93,57 +114,60 @@ class PetApp(object):
         return field_dict
 
     @staticmethod
-    def normalize_name(name):
+    def _normalize_name(name):
         "convert underscores to spaces and use title case"
         name = name.replace('_',' ').title()
         return name
 
-    def get_species(self, species_arg):
+    def _get_species(self, species_arg):
         """
         convert a species string to an instantiated species object
         - optionally creates a new species in the db if need be
         """
-        species_name = self.normalize_name( species_arg )
-        species = self.dbs.query(Species).filter(Species.name==species_name).first()
+        species_name = self._normalize_name( species_arg )
+        species = self._dbs.query(Species).filter(Species.name==species_name).first()
         if not species:
             species = Species(name=species_name)
-            self.dbs.add(species)
+            self._dbs.add(species)
         return species
 
 
-    def get_breed(self, breed_arg, species=None):
+    def _get_breed(self, breed_arg, species=None):
         """
         convert a breed string to an instantiated breed object
         takes an optional species param
         optionally creates a new breed in the db if need be
         """
-        breed_name = self.normalize_name( breed_arg )
-        breed = self.dbs.query(Breed).filter(Breed.name==breed_name).first()
+        breed_name = self._normalize_name( breed_arg )
+        breed = self._dbs.query(Breed).filter(Breed.name==breed_name).first()
         # we can only make a new breed if we got a species arg
         if not breed and species != None:
             breed = Breed(name=breed_name, species=species)
-            self.dbs.add(breed)
+            self._dbs.add(breed)
         # NB: we could be returning None for breed, that's ok
         return breed
 
 
-    def get_shelter(self, shelter_arg):
+    def _get_shelter(self, shelter_arg):
         """
         convert a shelter string to an instantiated shelter object
         - optionally creates a new shelter in the db if need be
         """
         # we use the shelter name as is, no normalizing
-        shelter = self.dbs.query(Shelter).filter(
+        shelter = self._dbs.query(Shelter).filter(
             Shelter.name==shelter_name).first()
         if not shelter:
             shelter = Shelter(name=shelter_name)
-            self.dbs.add(shelter)
+            self._dbs.add(shelter)
         return shelter
 
 
     @classmethod
-    def search_output(cls, pets):
-        "create the string output from a list of pets"
+    def _search_output(cls, pets):
+        """
+        create the string output from a list of pets
+        param pets: a list of pet objects to display
+        """
         # NB: this could be unit tested without the database
         # as we only need the pets param to contain objects with
         # the right attributes
@@ -162,41 +186,47 @@ class PetApp(object):
         return output     
 
 
-    def output_new_pet(self, pet):
+    @classmethod
+    def _add_output(cls, pet):
         "create the string output for a new pet creation"
         output = ("New pet created. Name: %s Age: %s: Adopted: %s Shelter: %s"
             % (pet.name, pet.age, pet.adopted, pet.shelter) )
         return output
 
 
-    def save_pet(self, fields):
-        "persist a pet to the DB from a dict of values"
+    def _save_pet(self, fields):
+        """
+        Persist a pet to the DB from a dict of values
+        param fields: dict of field values for the new pet
+        returns:  newly created pet
+        side effects: new pet saved in database
+        """
         new_pet = Pet()
         for attr,value in fields.items():
             if hasattr(Pet, attr):
                 setattr(new_pet, attr, value)
-        self.dbs.add( new_pet )
-        self.dbs.commit()
+        self._dbs.add( new_pet )
+        self._dbs.commit()
         return new_pet 
             
 
-    def get_pets(self, filter_dict):
+    def _get_pets(self, filter_dict):
         "return a list of Pet objects for a filter dict"
         
         # make our base query
-        query = self.dbs.query(Pet)
+        query = self._dbs.query(Pet)
         # filter for breed
         if 'breed' in filter_dict:
             query = query.join(Breed)
             # convert "labrador_retriever" to "Labrador Retriever"
             # we use dict.pop to remove the item from the dict
-            breed_name = self.normalize_name( filter_dict.pop('breed') )
+            breed_name = self._normalize_name( filter_dict.pop('breed') )
             query = query.filter(Breed.name==breed_name)
 
         # only filter on species if not filtering on breed
         if 'species' in filter_dict and 'breed' not in filter_dict:
             query = query.join(Breed).join(Species)
-            species_name = self.normalize_name( filter_dict.pop('species') )
+            species_name = self._normalize_name( filter_dict.pop('species') )
             query = query.filter(Species.name == species_name) 
        
         # shelter
